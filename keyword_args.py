0d9c57bb278911a6f19654f0e43325b81b533e5d
tiffanyv185 <tiffanyv185@gmail.com>
2024-06-13 19:42:24 +1000

    recap on functions

---
+++ keyword_args.py
@@ -0,0 +1,13 @@
+def greet(fname, lname):
+    print(Hello, {fname}, {lname}!)
+
+greet("Max", "Brenner")
+
+# using keyword arguments
+
+greet(fname = "Max", lname = "Brenner")
+
+greet (lname = "Brenner", fname = "max")
+
+def describe_
+ 
